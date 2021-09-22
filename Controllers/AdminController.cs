b959f6bff79e50542d432c14072c89e41d8c534f
Ben Jaber <sunghsm@gmail.com>
2021-09-22 17:08:56 +0100

    first commit

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Atlas;
+
+namespace Atlas.Controllers
+{
+    public class AdminController : Controller
+    {
+        private EntityDtsEntities db = new EntityDtsEntities();
+
+        // GET: Admin
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.iOrders.ToListAsync());
+        }
+
+        // GET: Admin/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            iOrders iOrders = await db.iOrders.FindAsync(id);
+            if (iOrders == null)
+            {
+                return HttpNotFound();
+            }
+            return View(iOrders);
+        }
+
+        // GET: Admin/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "id,product_id,account_id,order_delivery,order_price,product_name")] iOrders iOrders)
+        {
+            if (ModelState.IsValid)
+            {
+                db.iOrders.Add(iOrders);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(iOrders);
+        }
+
+        // GET: Admin/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            iOrders iOrders = await db.iOrders.FindAsync(id);
+            if (iOrders == null)
+            {
+                return HttpNotFound();
+            }
+            return View(iOrders);
+        }
+
+        // POST: Admin/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "id,product_id,account_id,order_delivery,order_price,product_name")] iOrders iOrders)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(iOrders).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(iOrders);
+        }
+
+        // GET: Admin/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            iOrders iOrders = await db.iOrders.FindAsync(id);
+            if (iOrders == null)
+            {
+                return HttpNotFound();
+            }
+            return View(iOrders);
+        }
+
+        // POST: Admin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            iOrders iOrders = await db.iOrders.FindAsync(id);
+            db.iOrders.Remove(iOrders);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+namespace Atlas.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Atlas.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "Atlas.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(Atlas.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
